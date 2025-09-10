c397e94763a9cc5dac650b13dd108fc42db9c941
William Christian <wllchrst@outlook.com>
2025-09-10 14:14:03 +0700

    fix: remove interface from previous project

--- llm/base_llm.py
+++ llm/base_llm.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from interfaces import IDocument
 class BaseLLM(ABC):
     def __init__(self):
         super().__init__()
@@ -7,25 +6,3 @@ class BaseLLM(ABC):
     @abstractmethod
     def answer(self, query: str):
         pass
-    
-    def format_with_document(self, prompt: str, documents: list[IDocument]) -> str:
-        """
-        Formats the prompt with the document content.
-        """
-        context_formatted = ""
-        for doc in documents:
-            context_formatted += f"Context Title: {doc.metadata.title}\n{doc.text}\n\n"
-        
-        return "\n".join([
-            context_formatted,
-            f"Q: {prompt}",
-            "Berikan jawaban yang singkat."
-        ])
-    
-    def validate_context(self, query: str, context:str) -> bool:
-        question = f"Apakah konteks berikut cukup untuk menjawab pertanyaan: '{query}'?\n\nKonteks:\n{context}\n\nJawab dengan 'Ya' atau 'Tidak'."
-
-        response = self.answer(question)
-        print(response)
-        
-        return True if "Ya" in response else False
