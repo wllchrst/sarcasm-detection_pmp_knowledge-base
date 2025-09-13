f17a78783ffd4c27009f737bd344b3f2bef2c037
William Christian <wllchrst@outlook.com>
2025-09-13 10:50:27 +0700

    feat: to do list and example of ner entry

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 ﻿def main():
-    pass
+    from ner import NEREntry
 
+    entry = NEREntry()
+    context = entry.get_sentence_context("I think Elon Musk is really needs more money to buy food")
+    print(context)
+
+# TODOLIST
+# 1. Finish NER Evaluation to give context of the sentence
+# 2. Create Evaluation pipeline.
+# 3. Add logging using MLFLOW
 if __name__ == "__main__":
     main()
