428f8848e58e0407cfe8501221f77d88f2cd76f2
William Christian <wllchrst@outlook.com>
2025-09-13 11:34:06 +0700

    feat: running system evaluation with arguments

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-﻿def main():
-    from ner import NEREntry
+﻿import argparse
 
-    entry = NEREntry()
-    context = entry.get_sentence_context("I think Elon Musk is really needs more money to buy food")
-    print(context)
+parser = argparse.ArgumentParser(description="Python script that is used for indo adaptive rag experiments")
 
-# TODOLIST
-# 1. Finish NER Evaluation to give context of the sentence
-# 2. Create Evaluation pipeline.
-# 3. Add logging using MLFLOW
+def parse_all_args():
+    parser.add_argument("--dataset", help="Dataset that is going to evaluated")
+    parser.add_argument("--llm_model", help="Dataset that is going to evaluated")
+    parser.add_argument("--sentiment_model", help="Dataset that is going to evaluated")
+    parser.add_argument("--use_context", help="Is the evaluation going to be run using the context of the dataset", action='store_true')
+    parser.add_argument("--with_logging", help="Is the evaluation going to log it into terminal", action='store_true')
+    return parser.parse_args()
+
+def main():
+    from evaluation_system import System
+    from interfaces import SystemArgument
+    arguments = parse_all_args()
+
+    system_argument = SystemArgument(
+        dataset=arguments.dataset,
+        use_context=arguments.use_context,
+        with_logging=arguments.with_logging,
+        llm_model=arguments.llm_model,
+        sentiment_model=arguments.sentiment_model
+    )
+
+    system = System(system_argument)
+    result = system.evaluate()
+    print(result)
+
 if __name__ == "__main__":
     main()
