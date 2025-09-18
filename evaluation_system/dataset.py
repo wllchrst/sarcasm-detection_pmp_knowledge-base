163111752b3d751b5412fe43f57f4291adf34de6
William Christian <wllchrst@outlook.com>
2025-09-18 16:10:34 +0700

    feat: load semeval dataset with the id for saving the checkpoint

--- evaluation_system/dataset.py
+++ evaluation_system/dataset.py
@@ -1,13 +1,17 @@
 """
 Make sure all dataset that is loaded from this file is already processed by labeling the column name
 
+Also please make sure dataset that is loaded has id column to for saving checkpoint the result if there is an error.
+
 Label Column Name: 'label'
 Text Column Name: 'text'
 Context Column Name: 'context'
+ID Column Name: 'id'
 """
 import pandas as pd
 
+
 def load_semeval_dataset(file_path: str = 'SemEval2018-T3_gold_test_taskA_emoji.txt') -> pd.DataFrame:
     semeval_df = pd.read_csv(file_path, sep='\t')
-    
-    return semeval_df.rename(columns={'Tweet text': 'text', 'Label': 'label'})
+
+    return semeval_df.rename(columns={'Tweet text': 'text', 'Label': 'label', 'Tweet index': 'id'})
