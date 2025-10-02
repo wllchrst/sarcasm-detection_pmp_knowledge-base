056fceda8e6edd82013e3dc86332832f6f516c4f
William Christian <wllchrst@outlook.com>
2025-10-02 16:55:22 +0700

    feat: get unknown words done (just need to add get the definition)

--- generate_context.py
+++ generate_context.py
@@ -3,8 +3,11 @@ import os
 from ner.ner_processor import NERProcessor
 from evaluation_system.dataset import load_twitter_indonesian_dataset
 from interfaces import LLMType
+from typing import Optional
+from helpers.argument_helper import ArgumentHelper
 
 OUTPUT_FOLDER = 'twitter_with_context'
+
 
 def load_objects():
     processor = NERProcessor(
@@ -14,9 +17,9 @@ def load_objects():
         with_logging=False,
         model_name='qwen3:8b'
     )
-    train_df = load_twitter_indonesian_dataset('train')
-    validation_df = load_twitter_indonesian_dataset('validation')
-    test_df = load_twitter_indonesian_dataset('test')
+    train_df = load_twitter_indonesian_dataset(partition='train')
+    validation_df = load_twitter_indonesian_dataset(partition='validation')
+    test_df = load_twitter_indonesian_dataset(partition='test')
 
     dataframe_dict = {
         'train': train_df,
@@ -31,39 +34,91 @@ def detect_unknown_words(processor: NERProcessor,
                          dataframe: pd.DataFrame,
                          file_path: str = None) -> pd.DataFrame:
     unknown_words = []
+    texts = []
+    labels = []
+    ids = []
+
     saved_dataframe = None
     if os.path.exists(file_path):
         saved_dataframe = pd.read_csv(file_path)
 
-    for _, row in dataframe.iterrows():
-        text = row['text']
-        words = processor.get_unknown_words(text, unknown_words)
-        unknown_words.append(words)
-    
-    dataframe = dataframe.iloc[:len(unknown_words)].copy()
-    dataframe['unknown_words'] = unknown_words
+    predicted_ids = [] if saved_dataframe is None else saved_dataframe['id'].values
 
-    return dataframe
+    print(f'predicted_ids {predicted_ids}')
+
+    try:
+        for index, row in dataframe.iterrows():
+            print(index)
+            current_id = row['id']
+            text = row['text']
+            label = row['label']
+
+            ids.append(current_id)
+            texts.append(text)
+            labels.append(label)
+
+            if current_id in predicted_ids:
+                print(f'Skipping id {current_id}')
+                words = saved_dataframe.loc[saved_dataframe['id'] == current_id, 'unknown_words'].iloc[0]
+                unknown_words.append(words)
+            else:
+                words = processor.get_unknown_words(text=text)
+                unknown_words.append(words)
+
+    except Exception as e:
+        checkpoint_df = pd.DataFrame({
+            'id': ids,
+            'texts': texts,
+            'label': labels,
+            'unknown_words': unknown_words
+        })
+
+        checkpoint_df.to_csv(file_path, index=False)
+        raise e
+
+    return pd.DataFrame({
+        'id': ids,
+        'texts': texts,
+        'label': labels,
+        'unknown_words': unknown_words
+    })
+
 
 def generate_output_path(partition: str):
     os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
-
     return f'{OUTPUT_FOLDER}/{partition}.csv'
-    
 
-def start_generate_context():
+
+def start_generate_context(partition: Optional[str] = None):
     processor, dataframe_dictionary = load_objects()
+
+    if partition is not None:
+        if partition in dataframe_dictionary:
+            dataframe_dictionary = {
+                partition: dataframe_dictionary[partition]
+            }
+        else:
+            raise ValueError(f"Partition '{partition}' not found in dataframe_dictionary")
 
     for partition, df in dataframe_dictionary.items():
         print(f"Processing partition: {partition}")
 
-        df_with_unknowns = detect_unknown_words(processor, df)
         output_path = generate_output_path(partition)
+        df_with_unknowns = detect_unknown_words(
+            dataframe=df,
+            processor=processor,
+            file_path=output_path
+        )
+
         df_with_unknowns.to_csv(output_path, index=False)
 
         print(f"Saved processed data with unknown words to {output_path}")
 
 
 if __name__ == "__main__":
-    start_generate_context()
+    arguments = ArgumentHelper.parse_context_generation()
+
+    if arguments.partition is None:
+        raise ValueError("arguments.partition cannot be none")
+    start_generate_context(partition=arguments.partition)
